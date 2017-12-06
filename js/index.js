2ae3f34e360d20369a00e0aa2db7196d05da26db
kek <oleh.petryk@gmail.com>
2017-12-06 18:33:58 +0200

    c

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-
-const  userName = prompt("What is your name?", "Your name...");
+const userName = prompt("What is your name?", "Your name...");
 const greetings = "Hello " + userName;
 document.write(greetings);
+
+document.write("<br>");
+
+const userAge = prompt("What is youtr age?");
+const nextAge = +userAge + 1;
+document.write("Next year you will be "+ nextAge);
