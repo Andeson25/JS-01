97c4d2edeefde67df42157f0c898540e11946d0e
kek <oleh.petryk@gmail.com>
2017-12-06 19:19:26 +0200

    c

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 // const greetings = "Hello " + userName;
 // document.write(greetings);
 // const endl = "<br>";
-    // document.write(endl);
+// document.write(endl);
 
 // const userAge = parseFloat(prompt("What is youtr age?"));
 // const nextAge = userAge + 1;
@@ -17,8 +17,24 @@
 //     document.write(endl + "Adult");
 //   }
 // }
- function sum(a,b) {
-     return a+b;
- }
-
-document.write(sum(4,10));
+// function fun(a, b) {
+//   return a + b;
+// }
+// function sum(funPtr, a, b, c) {
+//   return funPtr(a, b) + a + b + c;
+// }
+// document.write(sum(fun, 10, 10, 10));
+const a = Number(prompt("Enter A"));
+const b = Number(prompt("Enter B"));
+const c = Number(prompt("Enter C"));
+function disc(_a, _b, _c) {
+  return _b ** 2  - 4 * _a * _c;
+}
+function solveQuadr(a,b,c) {
+  const d = disc(a, b, c);
+  const x1 = (-b + Math.sqrt(d)) / 2 * a;
+  const x2 = (-b - Math.sqrt(d)) / 2 * a;
+  var result = "x1="+x1 + " x2=" + x2;
+    return result;
+}
+document.write(solveQuadr(a,b,c));
