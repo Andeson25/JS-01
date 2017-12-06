e41e75b11272e0b92b6d3dad9204f618a2479df2
kek <oleh.petryk@gmail.com>
2017-12-06 18:15:32 +0200

    c

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 console.log("Hello world!");
+
 var messageForUser = "Hello World!";
 document.write(messageForUser);
+
 var userAge = 20;
 document.write("<br>" + messageForUser + "\n" + userAge);
+
 let userProfession = "Developer";
-document.write('<br>'+userProfession);
+document.write("<br>" + messageForUser + " " + userProfession + " " + userAge);
+
+const favouriteColor = "red";
