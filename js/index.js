829c392c0d49ce6404ac9388a259ff013986eb24
kek <oleh.petryk@gmail.com>
2017-12-08 16:20:06 +0200

    c

--- js/index.js
+++ js/index.js
@@ -1,34 +1,8 @@
-// const userName = prompt("What is your name?", "Your name...");
-// const greetings = "Hello " + userName;
-// document.write(greetings);
-// const endl = "<br>";
-// document.write(endl);
-
-// const userAge = parseFloat(prompt("What is youtr age?"));
-// const nextAge = userAge + 1;
-// document.write("You are  " + userAge+" years old");
-
-// if (isNaN(userAge)) {
-//   document.write(endl + "User age is Nan");
-// } else {
-//   if (userAge < 18) {
-//     document.write(endl + "Child");
-//   } else {
-//     document.write(endl + "Adult");
-//   }
-// }
-// function fun(a, b) {
-//   return a + b;
-// }
-// function sum(funPtr, a, b, c) {
-//   return funPtr(a, b) + a + b + c;
-// }
-// document.write(sum(fun, 10, 10, 10));
 
 function getValues() {
-  let a = parseFloat(document.getElementById("a").value); //Number(prompt("Enter A"));
-  let b = parseFloat(document.getElementById("b").value); //Number(prompt("Enter B"));
-  let c = parseFloat(document.getElementById("c").value); //Number(prompt("Enter C"));
+  let a = parseFloat(document.getElementById("a").value); 
+  let b = parseFloat(document.getElementById("b").value); 
+  let c = parseFloat(document.getElementById("c").value); 
   if (isNaN(a) || isNaN(b) || isNaN(c)) {
     document.getElementById("solve").innerHTML = " ";
     alert("Input values please!");
